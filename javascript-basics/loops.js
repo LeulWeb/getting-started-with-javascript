f96d6f26b2d02c8270ddbfe2336fbc89063e7fb2
bishu <leulwebsdev@gmail.com>
2024-06-01 18:30:19 +0300

    loop

---
+++ javascript-basics/loops.js
@@ -0,0 +1,84 @@
+// what is loop ?
+// loop is something that repeatedly performs an action
+
+// 1. for loop 
+/* 
+    for(initialize; condition; increment){
+
+    }
+
+*/
+
+
+// for(let count = 1; count<=100; count++){
+//     console.log("Grimay")
+// }
+
+// infinite loop
+// for(let index = 1 ; index>0; index++){
+//     console.log("Hello")
+// }
+
+
+// print 1- 5
+// for(let i = 1; i<=5 ; i++){
+//     console.log(i)
+// }
+
+
+// print 5 -1
+// for(let i = 5; i>=1; i--){
+//     console.log(i)
+// }
+
+
+
+// Nesting 
+
+// for(let i = 0 ; i <=5 ; i++){
+//     for(let j= 0 ; j<= i; j++){
+//         console.log(i+"5")
+//     }
+// }
+
+
+
+// while loop
+/* 
+
+while(condition){
+    //our code 
+    //our increment
+}
+1*/
+
+// let counter = 0
+// while(counter<5){
+//     console.log("Eyobel")
+//     counter++
+// }
+
+
+let index = 1
+while(index > 2){
+    index++
+    console.log(index)
+}
+
+// 3rd loop type
+
+// do {
+    
+// } while (condition);
+
+
+// do {
+//   index++;
+//   console.log(index);
+// } while (index > 2);
+
+
+// do{
+//     console.log(index)
+//     index++
+// }while(index<=5)
